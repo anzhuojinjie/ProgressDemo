7d66bba9669776b49585ce12dcc0b73c805c295e
joey <963893628@qq.com>
2017-09-22 15:04:19 +0800

    commit all

---
+++ app/src/main/java/com/joey/progressdemo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.joey.progressdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MyProgressBar  saleProgressView = (MyProgressBar ) findViewById(R.id.spv);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seek);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                saleProgressView.setTotalAndCurrentCount(100,i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
